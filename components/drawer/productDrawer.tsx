5ac03a1b3444ae341565e43ab805a4388ed39571
abuhasanmahmud <mahmud.me420@gmail.com>
2023-09-27 09:53:44 +0600

    feat(V-1) product page

--- components/drawer/productDrawer.tsx
+++ components/drawer/productDrawer.tsx
@@ -157,11 +157,11 @@ const ProductDrawer = ({ productDrawer, setProductDrawer }: any) => {
 
                         {/* product img */}
                         <div>
-                          <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                             Upload product image
                           </label>
                           <input
-                            class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                            className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                             id="product-img"
                             type="file"
                           />
