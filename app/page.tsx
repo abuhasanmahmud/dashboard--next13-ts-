90191d988da26ec5b8137e818c0daf0f7220b7af
abuhasanmahmud <mahmud.me420@gmail.com>
2023-10-13 19:32:00 +0600

    Feat(V-1)................

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import DashboardHome from "@/components/dashboard/DashboardHome";
 import Sidebar from "@/components/shared/Sidebar";
 
 export default function Home() {
-  console.log('product page')
+  // console.log('product page')
   return (
     <>
       <Sidebar component={<DashboardHome />} />
